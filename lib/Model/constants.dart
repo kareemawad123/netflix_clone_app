99ca5a25e857b4c030c3a104a13c93a7a089d5f9
Kareem Awad <kareemawad120@gmail.com>
2023-06-19 19:42:44 +0300

    add colors to constants

--- lib/Model/constants.dart
+++ lib/Model/constants.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-const bgColor = Colors.lightBlueAccent;
+const primaryColors = Color.fromARGB(255, 34, 31, 31);
+const secondaryColors = Color.fromARGB(255, 245, 245, 241);
+const tertiaryColors = Color.fromARGB(255, 00, 00, 00);
+const quaternaryColors = Color.fromARGB(255,229,9,20);
